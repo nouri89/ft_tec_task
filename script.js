410b36144f258c0446863a7a034943cc61d316ee
nouri89 <nouri89@hotmail.com>
2021-05-11 04:11:42 +0100

    work on styling

--- script.js
+++ script.js
@@ -12,26 +12,39 @@ async function fetchData() {
 	let mainDiv = document.createElement("div");
 	mainDiv.style.display = "flex";
 	mainDiv.style.marginTop = "2%";
-	//mainDiv.style.flexDirection = "row";
+
+	let titlesDev = document.createElement("div");
+
+	mainDiv.appendChild(titlesDev);
+	titlesDev.style.display = "flex";
+	titlesDev.style.flexDirection = "column";
+	titlesDev.style.margin = "1% 10% 1% 1%";
+	//titlesDev.style.backgroundColor = "green";
 
 	mainPage.appendChild(mainDiv);
 	let mainArticle = document.createElement("h1");
 	mainArticle.style.fontFamily = "Times New Roman, Times, serif";
-	mainDiv.appendChild(mainArticle);
+	titlesDev.appendChild(mainArticle);
 
 	mainArticle.innerText = results[0].title.title;
 
 	let mainArticleSummary = document.createElement("p");
-	mainArticleSummary.style.margin = "5% 5% 1% 1%";
-	mainDiv.appendChild(mainArticleSummary);
+	mainArticleSummary.style.margin = "1% 1%";
+	mainArticleSummary.style.color = "grey";
+	titlesDev.appendChild(mainArticleSummary);
 	mainArticleSummary.innerText = results[0].summary.excerpt;
 
 	let mainArticleImg = document.createElement("img");
 	mainDiv.appendChild(mainArticleImg);
-	mainArticleImg.width = "200";
-	mainArticleImg.height = "200";
-	mainArticleImg.style.float = "right";
+	mainArticleImg.width = "250";
+	mainArticleImg.height = "250";
+	mainArticleImg.style.margin = "1% 10% 1% 1%";
+
 	mainArticleImg.setAttribute("src", results[0].images[0].url);
+	let lineBraker = document.createElement("hr");
+
+	titlesDev.appendChild(lineBraker);
+	lineBraker.style.border = "1px solid black";
 
 	let secondDiv = document.createElement("div");
 	secondDiv.setAttribute("class", "row");
@@ -41,38 +54,38 @@ async function fetchData() {
 		let HorisontalDiv = document.createElement("div");
 		HorisontalDiv.setAttribute("class", "col-md-2 rounded ");
 		secondDiv.appendChild(HorisontalDiv);
-		let hrArticle = document.createElement("h6");
-		HorisontalDiv.appendChild(hrArticle);
-		hrArticle.innerText = results[i].title.title;
-
-		let hrArticleSummary = document.createElement("p");
-		HorisontalDiv.appendChild(hrArticleSummary);
-		mainArticleSummary.innerText = results[i].summary.excerpt;
 
 		let hrArticleImg = document.createElement("img");
 		HorisontalDiv.appendChild(hrArticleImg);
 		mainArticleImg.width = "250";
-
 		mainArticleImg.height = "200";
 		mainArticleImg.style.float = "right";
 		hrArticleImg.setAttribute("src", results[i].images[0].url);
-	}
+		hrArticleImg.style.marginBottom = "5%";
 
-	for (let j = 5; j < results.length; j++) {
-		let HorisontalDiv = document.createElement("div");
-		HorisontalDiv.setAttribute("class", "col-md-4 rounded ");
-		secondDiv.appendChild(HorisontalDiv);
-		let hrArticle = document.createElement("h6");
+		let hrArticle = document.createElement("h5");
+		hrArticle.style.fontFamily = "Times New Roman, Times, serif";
 		HorisontalDiv.appendChild(hrArticle);
-		hrArticle.innerText = results[j].title.title;
+		hrArticle.innerText = results[i].title.title;
 
 		let hrArticleSummary = document.createElement("p");
+		hrArticleSummary.style.fontSize = "75%";
+		hrArticleSummary.style.color = "grey";
+
 		HorisontalDiv.appendChild(hrArticleSummary);
-		mainArticleSummary.innerText = results[j].summary.excerpt;
+		hrArticleSummary.innerText = results[i].summary.excerpt;
+	}
+	let lineBraker2 = document.createElement("hr");
+
+	secondDiv.appendChild(lineBraker2);
+	lineBraker2.style.border = "1px solid black";
+
+	for (let j = 5; j < results.length; j++) {
+		let bottomDiv = document.createElement("div");
+		bottomDiv.setAttribute("class", "col-md-6 rounded ");
 
 		let hrArticleImg = document.createElement("img");
-		HorisontalDiv.appendChild(hrArticleImg);
-		console.log(typeof results[j].images[0]);
+		bottomDiv.appendChild(hrArticleImg);
 
 		if (typeof results[j].images[0] === undefined) {
 			hrArticleImg.setAttribute(
@@ -81,6 +94,28 @@ async function fetchData() {
 			);
 		} else if (typeof results[j].images[0] !== undefined) {
 			hrArticleImg.setAttribute("src", results[j].images[0].url);
+		}
+
+		secondDiv.appendChild(bottomDiv);
+		let hrArticle = document.createElement("h5");
+		hrArticle.style.fontFamily = "Times New Roman, Times, serif";
+		hrArticle.style.margin = "1% 1% 1% 30%";
+		bottomDiv.appendChild(hrArticle);
+		hrArticle.innerText = results[j].title.title;
+
+		let hrArticleSummary = document.createElement("p");
+		hrArticleSummary.style.fontFamily = "Times New Roman, Times, serif";
+		hrArticleSummary.style.margin = "1% 1% 1% 30%";
+		hrArticleSummary.style.color = "grey";
+		bottomDiv.appendChild(hrArticleSummary);
+		hrArticleSummary.innerHTML = "Text not available";
+		let lineBrake = document.createElement("hr");
+		bottomDiv.appendChild(lineBrake);
+
+		if (results[j].summary.excerpt === undefined) {
+			hrArticleSummary.innerText = "Text not available";
+		} else if (results[j].summary.excerpt !== undefined) {
+			hrArticleSummary.innerText = results[j].summary.excerpt;
 		}
 	}
 	document
@@ -108,14 +143,17 @@ async function fetchData() {
 			searchPanelFlag = false;
 
 			let searchedWord = document.querySelector("#searchWord").value;
-
+			if (searchedWord === "") {
+				alert("No Search word Entered! please enter a Key word");
+			}
 			document.getElementById("articles").innerHTML = "";
-			searchHeader();
+			searchHeader(results);
 			revealSearchResults(results, searchedWord);
 		});
 }
 
-function searchHeader() {
+function searchHeader(fetchedApi) {
+	const results = fetchedApi;
 	let searchArticle = document.getElementById("articles");
 	searchArticle.style.display = "flex";
 	searchArticle.style.flexDirection = "column";
@@ -126,22 +164,32 @@ function searchHeader() {
 	searchHead.appendChild(searchTitle);
 
 	let newSearchDiv = document.createElement("div");
+	newSearchDiv.style.display = "flex";
+	//newSearchDiv.style.justifyContent = "space-between";
+
 	searchHead.appendChild(newSearchDiv);
 
 	liveSearch = document.createElement("INPUT");
 	liveSearch.setAttribute("id", "newSearchHeadline");
+	liveSearch.style.width = "70%";
 	newSearchDiv.appendChild(liveSearch);
 	liveSearch.placeholder = "Please Enter a Search Term";
 	let newSearchIcon = document.createElement("i");
-	newSearchIcon.setAttribute("class", "fa fa-search");
+	newSearchIcon.setAttribute("class", "fa fa-search fa-2x"); //==============add eventlistner
+	let newSearchValue = document.querySelector("#newSearchHeadline").value;
+	newSearchIcon.addEventListener("click", () => {
+		document.getElementById("searchResultDiv").innerHTML = "";
+		//	searchHeader();
+		revealSearchResults(results, newSearchValue);
+		console.log("new search");
+	});
+	newSearchIcon.style.cursor = "pointer";
 	newSearchIcon.style.backgroundColor = "green";
-	newSearchIcon.style.color = "white";
-	newSearchDiv.appendChild(newSearchIcon);
+	newSearchIcon.style.margin = "1% 3% 1% 3%";
 
-	let progressDiv = document.createElement("div");
-	searchHead.appendChild(progressDiv);
-	progressDiv.style.backgroundColor = "black";
-	progressDiv.style.height = "1%";
+	newSearchDiv.style.margin = "1% 3% 1% 3%";
+
+	newSearchDiv.appendChild(newSearchIcon);
 
 	let searchResultDiv = document.createElement("div");
 	searchResultDiv.setAttribute("id", "searchResultDiv");
@@ -149,16 +197,12 @@ function searchHeader() {
 }
 
 function revealSearchResults(results, word) {
+	console.log("test1");
 	let searchRes = document.getElementById("searchResultDiv");
 
-	results.forEach((element, index) => {
-		console.log(typeof element.title.title);
-		console.log(typeof element.summary.excerpt);
-	});
 	const clearnResult = results.filter((element) => {
 		return typeof element.summary.excerpt === "string";
 	});
-	console.log(clearnResult.length);
 
 	const articlesFound = clearnResult.filter((element) => {
 		return (
@@ -167,9 +211,11 @@ function revealSearchResults(results, word) {
 		);
 	});
 
+	console.log(articlesFound.length);
 	articlesFound.forEach((element, index) => {
 		let singleResultDiv = document.createElement("div");
 		singleResultDiv.setAttribute("id", "singleResultDiv");
+		singleResultDiv.style.padding = "1% 1% ";
 
 		if (element.images[0] !== undefined) {
 			let resultImg = document.createElement("img");
@@ -178,18 +224,27 @@ function revealSearchResults(results, word) {
 			singleResultDiv.appendChild(resultImg);
 		}
 
-		let resultheader = document.createElement("h4");
+		let resultheader = document.createElement("h5");
+		resultheader.style.fontFamily = "Times New Roman, Times, serif";
+		resultheader.style.marginLeft = "20%";
+
 		resultheader.innerText = element.title.title;
 		singleResultDiv.appendChild(resultheader);
 
 		let resultSummary = document.createElement("p");
 		resultSummary.innerText = element.summary.excerpt;
+		resultSummary.style.fontFamily = "Times New Roman, Times, serif";
+		resultSummary.style.marginLeft = "20%";
+		resultSummary.style.color = "grey";
 		singleResultDiv.appendChild(resultSummary);
 
 		let articleDate = document.createElement("h6");
 		articleDate.innerText = element.lifecycle.initialPublishDateTime
 			.toString()
 			.slice(0, 10);
+		articleDate.style.fontFamily = "Times New Roman, Times, serif";
+		articleDate.style.marginLeft = "20%";
+		articleDate.style.color = "grey";
 		singleResultDiv.appendChild(articleDate);
 		searchRes.appendChild(singleResultDiv);
 
